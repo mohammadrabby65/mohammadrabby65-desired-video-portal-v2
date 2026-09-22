c5853b1e57ff3078d860dd3823560bcabbcd34c5
mohammadrabby65 <158773953+mohammadrabby65@users.noreply.github.com>
2026-09-22 10:05:29 +0600

    refactor: remove popunder script for security
    Removed the popunder script to prevent remote browser history hijacking and unauthorized navigation.

--- src/components/layout/ScriptManager.tsx
+++ src/components/layout/ScriptManager.tsx
@@ -4,8 +4,6 @@ import { db } from "../../lib/firebase";
 
 const SOCIAL_BAR_SRC =
   "https://pl30417136.effectivecpmnetwork.com/a8/c5/ae/a8c5ae6b95183bffe51c005c71b9acfd.js";
-const POPUNDER_SRC =
-  "https://predestineheadypleasure.com/46/fb/02/46fb02b7663603a5ec0e75ce574d43f4.js";
 
 export function ScriptManager() {
   const injected = useRef(false);
@@ -29,9 +27,7 @@ export function ScriptManager() {
           if (data.socialBarEnabled) {
             injectScript(SOCIAL_BAR_SRC);
           }
-          if (data.popunderEnabled) {
-            injectScript(POPUNDER_SRC);
-          }
+          // Note: Popunder script removed permanently as it contains remote backunder history hijacking
         } else {
           injected.current = true;
         }
